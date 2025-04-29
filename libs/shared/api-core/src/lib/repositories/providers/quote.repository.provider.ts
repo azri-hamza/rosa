b1e6fe91eb9119f5bab9f33345eff2505c150717
Hamza <azri.hamza@gmail.com>
2025-04-29 10:11:51 +0100

    fix typo

--- libs/shared/api-core/src/lib/repositories/providers/quote.repository.provider.ts
+++ libs/shared/api-core/src/lib/repositories/providers/quote.repository.provider.ts
@@ -2,7 +2,7 @@ import { Provider } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { createQuoteRepository } from '../quote.repository';
 
-export const QUOTE_REPOSITORY = Symbol('USER_REPOSITORY');
+export const QUOTE_REPOSITORY = Symbol('QUOTE_REPOSITORY');
 
 export const QuoteRepositoryProvider: Provider = {
   provide: QUOTE_REPOSITORY,
